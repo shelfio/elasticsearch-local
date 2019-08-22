3dc4da427dc983e33da801a53b570d960127af3e
Vitalii Sikora <vitalii.sikora@gmail.com>
2019-08-22 16:59:25 +0300

    feat: add start/stop es-local methods

---
+++ src/index.test.ts
@@ -0,0 +1,74 @@
+import {execSync} from 'child_process';
+import {start, stop} from '.';
+
+it('should start ElasticSearch v7.3.0 locally', async () => {
+  await start({esVersion: '7.3.0', port: 9000});
+
+  const response = await execSync('curl -s -X GET "localhost:9000/?pretty"');
+
+  expect(JSON.parse(response.toString())).toEqual({
+    name: 'es-local',
+    cluster_name: 'es-local',
+    cluster_uuid: expect.any(String),
+    version: {
+      number: '7.3.0',
+      build_flavor: 'default',
+      build_type: 'tar',
+      build_hash: expect.any(String),
+      build_date: expect.any(String),
+      build_snapshot: false,
+      lucene_version: '8.1.0',
+      minimum_wire_compatibility_version: '6.8.0',
+      minimum_index_compatibility_version: '6.0.0-beta1'
+    },
+    tagline: 'You Know, for Search'
+  });
+});
+
+it('should start ElasticSearch v6.8.2 locally', async () => {
+  await start({esVersion: '6.8.2', port: 9100});
+
+  const response = await execSync('curl -s -X GET "localhost:9100/?pretty"');
+
+  expect(JSON.parse(response.toString())).toEqual({
+    name: 'es-local',
+    cluster_name: 'es-local',
+    cluster_uuid: expect.any(String),
+    version: {
+      number: '6.8.2',
+      build_flavor: 'default',
+      build_type: 'tar',
+      build_hash: expect.any(String),
+      build_date: expect.any(String),
+      build_snapshot: false,
+      lucene_version: '7.7.0',
+      minimum_wire_compatibility_version: '5.6.0',
+      minimum_index_compatibility_version: '5.0.0'
+    },
+    tagline: 'You Know, for Search'
+  });
+});
+
+it('should start ElasticSearch v5.6.16 locally', async () => {
+  await start({esVersion: '5.6.16', port: 9200});
+
+  const response = await execSync('curl -s -X GET "localhost:9200/?pretty"');
+
+  expect(JSON.parse(response.toString())).toEqual({
+    name: 'es-local',
+    cluster_name: 'es-local',
+    cluster_uuid: expect.any(String),
+    version: {
+      number: '5.6.16',
+      build_hash: expect.any(String),
+      build_date: expect.any(String),
+      build_snapshot: false,
+      lucene_version: '6.6.1'
+    },
+    tagline: 'You Know, for Search'
+  });
+});
+
+afterEach(async () => {
+  await stop();
+});
---
+++ src/index.ts
@@ -0,0 +1,119 @@
+import getDebug from 'debug';
+import {execSync} from 'child_process';
+import {promisify} from 'util';
+import {access, constants} from 'fs';
+import cwd from 'cwd';
+
+const debug = getDebug('elasticsearch-local');
+const FILEPATH_PREFIX = `${cwd()}/node_modules/.cache/@shelf/elasticsearch-local`;
+
+interface StartESOptions {
+  // Choose ES proper version at https://www.elastic.co/downloads/past-releases#elasticsearch
+  esVersion: string;
+  clusterName?: string;
+  nodeName?: string;
+  port?: number;
+  indexes?: ESIndex[];
+}
+
+interface ESIndex {
+  name: string;
+  // Body, which will be sent ot create index, see https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html#indices-create-index
+  body: object;
+}
+
+export async function start(options: StartESOptions): Promise<void> {
+  const {
+    esVersion,
+    clusterName = 'es-local',
+    nodeName = 'es-local',
+    port = 9200,
+    indexes = []
+  } = options;
+  const esURL = `http://localhost:${port}/`;
+  const isAfter7Version = esVersion[0] === '7';
+  const after7VersionSuffix = '-linux-x86_64';
+  const filenameSuffix = isAfter7Version ? after7VersionSuffix : '';
+  const esDownLoadURLPrefix = `https://artifacts.elastic.co/downloads/elasticsearch`;
+  const esArchiveFilepath = `${FILEPATH_PREFIX}/elasticsearch-${esVersion}${filenameSuffix}.tar.gz`;
+  const esDownloadURL = `${esDownLoadURLPrefix}/elasticsearch-${esVersion}${filenameSuffix}.tar.gz`;
+  const esBinaryFilepath = `${FILEPATH_PREFIX}/elasticsearch-${esVersion}/bin/elasticsearch`;
+  const esChecksumFilepath = `${FILEPATH_PREFIX}/elasticsearch-${esVersion}${filenameSuffix}.tar.gz.sha512`;
+  const esChecksumDownloadURL = `${esDownLoadURLPrefix}/elasticsearch-${esVersion}${filenameSuffix}.tar.gz.sha512`;
+
+  if (!esVersion) {
+    throw new Error('Please provide ElasticSearch version to start it locally');
+  }
+
+  if (!(await isExistingFile(esArchiveFilepath))) {
+    await execSync(`wget -P ${FILEPATH_PREFIX} ${esDownloadURL}`);
+    debug('Downloaded ES');
+  } else {
+    debug('ES already downloaded');
+  }
+
+  if (!(await isExistingFile(esChecksumFilepath))) {
+    await execSync(`wget -P ${FILEPATH_PREFIX} ${esChecksumDownloadURL}`);
+    debug('Downloaded checksum');
+
+    await execSync(`shasum -a 512 -c ${esChecksumFilepath}`);
+    debug('Checksum matched');
+  } else {
+    debug('Checksum already downloaded');
+  }
+
+  if (!(await isExistingFile(esBinaryFilepath))) {
+    await execSync(`tar -xzf ${esArchiveFilepath} -C ${FILEPATH_PREFIX}`);
+    debug('Unarchived ES');
+  } else {
+    debug('ES already unarchived');
+  }
+
+  debug('Starting ES');
+
+  await execSync(
+    `${esBinaryFilepath} -d -p ${FILEPATH_PREFIX}/elasticsearch-${esVersion}/es-pid -Ecluster.name=${clusterName} -Enode.name=${nodeName} -Ehttp.port=${port}`
+  );
+  debug('ES is running');
+
+  await Promise.all(
+    indexes.map(({name, body}) =>
+      execSync(
+        `curl -X PUT "${esURL}${name}" -H 'Content-Type: application/json' -s -d '${JSON.stringify(
+          body
+        )}'`
+      )
+    )
+  );
+  debug(`Created ${indexes.length} indexes`);
+
+  process.env.ES_URL = esURL;
+  process.env.ES_VERSION = esVersion;
+  process.env.ES_INDEXES_NAMES = JSON.stringify(indexes.map(({name}) => name));
+}
+
+export async function stop(): Promise<void> {
+  const indexes = JSON.parse(process.env.ES_INDEXES_NAMES).join(',');
+  const esURL = process.env.ES_URL;
+  const esVersion = process.env.ES_VERSION;
+
+  if (indexes) {
+    await execSync(`curl -XDELETE ${esURL}${indexes} -s`);
+    debug('Removed all indexes');
+  }
+
+  await execSync(`pkill -F ${FILEPATH_PREFIX}/elasticsearch-${esVersion}/es-pid`);
+  debug('ES has been stopped');
+}
+
+async function isExistingFile(filepath: string): Promise<boolean> {
+  const fsAccessPromisified = promisify(access);
+
+  try {
+    await fsAccessPromisified(filepath, constants.F_OK);
+
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
